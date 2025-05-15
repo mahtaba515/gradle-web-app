f1ad702cbb78f2b8678e818b19a543b5fbba6703
Mahtab <mahtaba515@gmail.com>
2025-05-16 00:23:22 +0530

    first commit

---
+++ app/src/main/java/com/raghu/MyServlet.java
@@ -0,0 +1,22 @@
+package com.raghu;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/")
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>WELCOME TO GRADLE </h1><h2> Mr. RAGHU </h2>");
+        out.println("</body></html>");
+    }
+}
